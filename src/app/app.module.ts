83d307b85b20f81a2b0c5cb9a31d545bc73524e0
Jordan Travaux <30296275+JTravaux@users.noreply.github.com>
2021-02-01 16:19:11 -0500

    Add Loading Indicators (#71)
    * initial setup and mining page
    
    * staking, auction loaders
    
    * wip
    
    * fix build
    
    Co-authored-by: Manty <Mvii7@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { MinMaxDirective } from "./directives/minmax/minmax";
 import { AngularFittextModule } from "angular-fittext";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
-import { MatDialogModule, MatTooltipModule, MAT_HAMMER_OPTIONS  } from "@angular/material";
+import { MatDialogModule, MatProgressSpinnerModule, MatTooltipModule, MAT_HAMMER_OPTIONS  } from "@angular/material";
 import { AppConfig } from "./appconfig";
 import { MiningPageComponent } from "./pages/mining-page/mining-page.component";
 
@@ -58,7 +58,7 @@ registerLocaleData(localeGB, "en-gb");
     BigNumberMax,
     StakingPageComponent,
     MinMaxDirective,
-    MiningPageComponent
+    MiningPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -71,6 +71,7 @@ registerLocaleData(localeGB, "en-gb");
     HttpClientModule,
     BrowserAnimationsModule,
     ClipboardModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     LocaleService,

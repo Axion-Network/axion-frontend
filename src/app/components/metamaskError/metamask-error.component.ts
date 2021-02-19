878bcb70234b6fcefc718c9f445862d5fb5d9866
Jordan <jordan.travaux@hotmail.com>
2021-02-19 09:01:19 -0500

    mention speed up

--- src/app/components/metamaskError/metamask-error.component.ts
+++ src/app/components/metamaskError/metamask-error.component.ts
@@ -15,7 +15,9 @@ export class MetamaskErrorComponent {
         this.err = data.msg;
 
         if (this.err.toLowerCase().includes("transaction was not mined within 50 blocks")) {
-            this.err = "Your transaction is taking longer than usual. Please check your wallet address on Etherscan and click on the pending transaction for an estimated transaction time. Do not cancel the transaction yet.";
+            this.err = `Your transaction is taking longer than usual. 
+                        Please check your wallet address on Etherscan and click on the pending transaction for an estimated transaction time. 
+                        Do not cancel the transaction yet. Alternatively, you can try to 'speed up' the transaction in the 'Activity' section of Metamask.`;
         }
     }
 
